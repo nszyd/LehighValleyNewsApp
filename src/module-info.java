290866926bd5e16db071f4bf2a620d8e1b4e5825
nszyd <113160580+nszyd@users.noreply.github.com>
2023-11-06 16:17:10 -0500

    Migrated to IntelliJ
    Migrated the program to development within IntelliJ
    
    Current Status:
    -Removed JUnit tests due to JUnit errors
    -Requires JavaFX, JSoup to run
    -Run configuration must be created

--- src/module-info.java
+++ src/module-info.java
@@ -1,7 +1,7 @@
 module LehighValleyNewsApp {
 	requires javafx.controls;
 	requires junit;
-	requires org.junit.jupiter.api;
+	//requires org.junit.jupiter.api;
 	requires org.jsoup;
 	exports model;
 	
